a151c9fb425eee57e2c43f4c92857e1ecbb35ee7
Phillip Dominguez <Padom02@gmail.com>
2024-05-31 02:51:57 -0400

    Fixed tokenizing behavior for keywords

--- source/TokenType.cpp
+++ source/TokenType.cpp
@@ -17,9 +17,6 @@ std::regex right_curly("\\}");
 std::regex semicolon(";");
 std::regex dot("\\.");
 
-//Identifier
-std::regex identifier("[a-z]([a-zA-Z0-9_])*");
-
 //Keywords
 std::regex if_key("if");
 std::regex else_key("else");
@@ -27,6 +24,10 @@ std::regex while_key("while");
 std::regex for_key("for");
 std::regex and_key("and");
 std::regex or_key("or");
+
+//Identifier
+std::regex identifier("[a-z]([a-zA-Z0-9_])*");
+
 
 //Map shit
 std::unordered_map<TOKEN_TYPES, std::regex> tokenTypeMap = {
@@ -42,8 +43,6 @@ std::unordered_map<TOKEN_TYPES, std::regex> tokenTypeMap = {
     {RIGHT_CURLY_BRACE, right_curly},
     {SEMICOLON, semicolon},
     {DOT, dot},
-    
-    {IDENTIFER, identifier},
 
     {IF, if_key},
     {ELSE, else_key},
@@ -51,6 +50,9 @@ std::unordered_map<TOKEN_TYPES, std::regex> tokenTypeMap = {
     {FOR, for_key},
     {AND, and_key},
     {OR, or_key},
+    
+    {IDENTIFER, identifier},
+
     
 
 };
